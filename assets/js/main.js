21f50298d607a4a7f5d34d1843f63008feab1f4b
alessiavozzo <a.vozzo95@gmail.com>
2024-02-22 14:38:24 +0100

    add email ex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log("ok");
+// mail ex
+
+const userEmail = prompt("Inserisci la tua email");
+
+const emailList = ["a.vozzo95@gmail.com", "alessia2@gmail.com","funziono@funziono.com"];
+
+let access = false;
+
+for (let i = 0; i < emailList.length; i++) {
+    if(emailList[i] === userEmail){
+        access = true;
+    }    
+    
+}
+
+console.log(access);
