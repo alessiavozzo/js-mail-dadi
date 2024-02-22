a7125b63f31d6e1747e7f719fff238da6f6a3503
alessiavozzo <a.vozzo95@gmail.com>
2024-02-22 16:24:55 +0100

    add colors mail-ex

--- email/assets/js/main.js
+++ email/assets/js/main.js
@@ -9,10 +9,17 @@ const loginBtn = document.querySelector(".login-btn")
 const emailInput = document.getElementById("email");
 //console.log(typeof emailInput);
 
+const h1Element = document.querySelector("h1");
+
+const okMessage = "You can access!"
+const noMessage = "You shall not pass!"
+
 /* add event listener */
 
-loginBtn.addEventListener("click", function(){
+loginBtn.addEventListener("click", function(e){
     
+    e.preventDefault()
+
     const userEmail = emailInput.value;
     //console.log(typeof userEmail);
     //console.log(userEmail);
@@ -26,13 +33,23 @@ loginBtn.addEventListener("click", function(){
     } 
     
     console.log(access);
+
+    let returnMessage;
     
     if(access === true){
         console.log("You can access");
+        returnMessage = okMessage;
+        h1Element.classList.add("text-white");
+        h1Element.classList.remove("text-danger", "display-3", "fw-bold");
     }
     
     else{
         console.log("You shall not pass");
+        returnMessage = noMessage;
+        h1Element.classList.add("text-danger", "display-3", "fw-bold");
+        h1Element.classList.remove("text-white");
     }
-
+    
+    h1Element.innerHTML = returnMessage
+    
 })
